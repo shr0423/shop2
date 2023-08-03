922e1afe4901ed59d820af0be89491eea20810d9
shr0423 <song8738@gmail.com>
2023-08-03 11:13:46 +0900

    shop0803

---
+++ src/org/sp/shop/network/ClientMain.java
@@ -0,0 +1,128 @@
+package org.sp.shop.network;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ClientMain extends JFrame{
+	JPanel p_north;
+	JComboBox box;
+	JTextField t_port;
+	JButton bt;
+	
+	JTextArea area;
+	JScrollPane scroll;
+	
+	JTextField t_input;
+	Socket socket;
+	ClientMessageThread cmt;
+	
+	public ClientMain() {
+		p_north = new JPanel();
+		box = new JComboBox();
+		t_port = new JTextField("7777", 6);
+		bt = new JButton("접속");
+		
+		area = new JTextArea();
+		scroll = new JScrollPane(area);
+		
+		t_input = new JTextField();
+		
+		//아이피 채워넣기
+		box.addItem("192.168.1.226");
+		
+		//조립 
+		p_north.add(box);
+		p_north.add(t_port);
+		p_north.add(bt);
+		add(p_north, BorderLayout.NORTH);
+		
+		add(scroll);
+		add(t_input, BorderLayout.SOUTH);
+		
+		setSize(300,400);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		bt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				connect();
+			}
+		});
+		
+		t_input.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				int key=e.getKeyCode();
+				if(key == KeyEvent.VK_ENTER) {//엔터치면..
+					//서버에 메시지 전송
+					send();
+				}
+			}
+		});
+	}
+	
+	//서버에 접속하기 
+	public void connect() {
+		String ip=(String)box.getSelectedItem();
+		int port=Integer.parseInt(t_port.getText());
+		
+		//소켓의  인스턴스가 생성될때 접속이 시도됨
+		try {
+			socket=new Socket(ip,port);
+			
+			//대화용쓰레드 생성
+			cmt=new ClientMessageThread(this);
+			cmt.start();//대화시작
+			
+			
+		} catch (UnknownHostException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	//서버에 메시지 보내기 
+	//회원제일경우 메시지뿐만 아니라 각종 부가적 정보도 함께 보내야 하기때문에
+	//구조화된 텍스트 기반의데이터를 표현할수있는 방법을 찾아야 한다.. json(아이디,이름,이모티콘(smile.png),메시지)
+	public void send() {
+		StringBuffer sb=new StringBuffer();
+		
+		//개발자가 전송 프로토콜을 정의한다.
+		sb.append("{");
+		sb.append("\"member_idx\":23,");
+		sb.append("\"id\" :\"zino\", ");
+		sb.append("\"name\" :\"지노\","); 
+		sb.append("\"icon\" : \"smile.png\", ");
+		sb.append("\"data\" :\""+t_input.getText()+"\" ");
+		sb.append("}");
+	
+		System.out.println(sb.toString());
+	
+		
+		cmt.sendMsg(sb.toString());
+		t_input.setText("");
+	}
+	
+	public static void main(String[] args) {
+		new ClientMain();
+	}
+}
---
+++ src/org/sp/shop/network/ClientMessageThread.java
@@ -0,0 +1,78 @@
+package org.sp.shop.network;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+//무한루프로 상대방의 메시지를 청취해야 하므로 쓰레드 이용 
+public class ClientMessageThread extends Thread{
+	ClientMain clientMain;
+	Socket socket;
+	BufferedReader buffr;//문자기반의 버퍼처리된 입력스트림
+	BufferedWriter buffw;//문자기반의 버퍼처리된 출력스트림
+	boolean flag=true;//이쓰레드를 죽일지 말지결정하는 논리값
+	
+	public ClientMessageThread(ClientMain clientMain) {
+		this.clientMain=clientMain;
+		this.socket=clientMain.socket;
+		try {
+			buffr=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			buffw=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	//듣고
+	public void listen() {
+		String msg=null;
+		try {
+			msg=buffr.readLine();
+			
+			//클라이언트가 json으로 프로토콜을 정의하여 전송했으므로
+			//서버에서는 이 json을 해석하여 원하는 데이터로 가공할 필요가 있음
+			JSONParser jsonParser=new JSONParser();
+			JSONObject jsonObject=(JSONObject)jsonParser.parse(msg);
+			//zino님:점심먹을래?
+			String id=(String)jsonObject.get("id");
+			String data=(String)jsonObject.get("data");
+			
+			String message=id+"님:"+data;
+			
+			clientMain.area.append(message+"\n");
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		} catch (ParseException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	//말하고
+	public void sendMsg(String msg) {
+		try {
+			buffw.write(msg+"\n");
+			buffw.flush();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(flag) {
+			listen();
+		}
+	}
+}
